8b3906a2090139c1ba6bb29109a47a40e590d60c
Mohammad Sanjaq <sanjaq.m@gmail.com>
2018-10-08 20:23:47 -0700

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+apt install build-essential
+apt install emacs
+apt install gcc
+apt install python3
+apt install valgrind
+apt install vim
+apt-get update
+apt-get upgrade
+
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+mv em /usr/bin
+rm "$0"
+mv * ..
